b890df7337086f1e6365044bf2ce663166cb7e7a
DIMEGLIOM <dimeglim@bc.edu>
2017-07-23 20:15:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ var api = new ParseServer({
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   
  
- // liveQuery: {
-  //  classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  // },
+ liveQuery: {
+    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+   },
   
   verifyUserEmails: true,
   publicServerURL: process.env.SERVER_URL || 'http://localhost:1337/parse',

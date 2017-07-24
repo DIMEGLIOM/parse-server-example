8ecb844e0525084381a8d26a4a2b2556a469aace
DIMEGLIOM <dimeglim@bc.edu>
2017-07-24 11:08:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'T2G1900',
-  masterKey: process.env.MASTER_KEY || 'T2G1metoread', //Add your master key here. Keep it secret!
+  appId: process.env.APP_ID || 'myAppId',
+  masterKey: process.env.MASTER_KEY || 'myMasterKey', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'https://localhost:1337/parse',  // Don't forget to change to https if needed
   
  
